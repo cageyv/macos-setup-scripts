c4f63e55db6651f2152aaac3ff814c49ac8f4ab1
Vladimir Samoylov <51059484+cageyv@users.noreply.github.com>
2022-02-05 00:04:19 +0700

    Update 01-core-tools.sh

--- 01-core-tools.sh
+++ 01-core-tools.sh
@@ -2,3 +2,10 @@
 brew install gpg2 gnupg pinentry-mac jq wget 
 # TODO: jre/java
 
+# Prepare for GPG
+mkdir ~/.gnupg
+touch ~/.gnupg/gpg.conf
+echo 'use-agent' > ~/.gnupg/gpg.conf
+echo 'export GPG_TTY=$(tty)' >> ~/.zshrc
+source ~/.zshrc
+chmod 700 ~/.gnupg
